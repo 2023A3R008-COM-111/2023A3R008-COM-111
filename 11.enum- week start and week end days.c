00730225533bacf8c9e49b089100dee8fb9799d3
2023A3R008-COM-111 <146550426+2023A3R008-COM-111@users.noreply.github.com>
2023-12-06 07:37:22 +0530

    Update 11.enum- week start and week end days.c

--- 11.enum- week start and week end days.c
+++ 11.enum- week start and week end days.c
@@ -2,29 +2,29 @@
 enum day{monday, tuesday, wednesday, thursday, friday, saturday};
 int main()
 {
-    enum day week_start, week_end;
+    enum day week_st, week_ed;
 
     printf("enter the start of the week (0=monday,1=tuesday,2=wednesday,3=thursday,4=friday,5=saturday,6=sunaday): ");
-    scanf("%d", &week_start);
+    scanf("%d", &week_st);
 
     printf("enter the end of the week (0=monday,1=tuesday,2=wednesday,3=thursday,4=friday,5=saturday,6=sunaday): ");
-    scanf("%d", &week_end);
+    scanf("%d", &week_ed);
 
-    if(week_start == tuesday)
+    if(week_st == tuesday)
     {
-        week_end = saturday;
+        week_ed = saturday;
     }
-    else if(week_start == monday)
+    else if(week_st == monday)
     {
-        week_end = friday;
+        week_ed = friday;
     }
     else
     printf("enter valid week start day\n");
 
     printf("user has entered following choices: \n");
 
-    printf("start of the week: %d\n",week_start);
-    printf("end of the week: %d\n", week_end);
+    printf("start of the week: %d\n",week_st);
+    printf("end of the week: %d\n", week_ed);
 
     return 0;
 }
