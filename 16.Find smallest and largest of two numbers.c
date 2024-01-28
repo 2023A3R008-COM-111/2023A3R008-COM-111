7b5e373adb7933e89726adabedc8a24d32446515
2023A3R008-COM-111 <146550426+2023A3R008-COM-111@users.noreply.github.com>
2024-01-28 14:48:33 +0530

    Update 16.Find smallest and largest of two numbers.c

--- 16.Find smallest and largest of two numbers.c
+++ 16.Find smallest and largest of two numbers.c
@@ -16,23 +16,7 @@ int main()
 	}else{
 		printf("%d is greater than %d.", numy, numx);
 	}
-	printf("\n");
-    
-	// To find Largest of two numbers
-	printf("Enter numx: ");
-	scanf("%d", &numx);
-
-	printf("Enter numy: ");
-	scanf("%d", &numy);
-
-	if(numx<numy){
-		printf("%d is smallest than %d.", numx, numy);
-	}else if(numx == numy){
-		printf("%d(numc) is equals to %d(numy).", numx, numy);
-	}else{
-		printf("%d is smallest than %d.", numy, numx);
-	}
-	printf("\n");
+	printf("\n");
 
 	return 0;
 }
